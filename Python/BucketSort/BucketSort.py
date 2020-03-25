21f845d972124bdbea2599009fbc3a6e1a3e23f4
sb17ho <sb17ho@brocku.ca>
2020-03-25 01:25:30 -0400

    Bucket Sort

--- Python/BucketSort/BucketSort.py
+++ Python/BucketSort/BucketSort.py
@@ -7,19 +7,44 @@ class Bucket:
 
 
 def BucketSort(bucket, intList):
+    newList = [None] * len(intList)
     for i in range(0, len(intList)):
         value = int(len(bucket) * intList[i])
         bucket[value].__add__(intList[i])
 
+    for i in range(0, len(bucket)):
+        InsertionSort(bucket[i].list)
 
-def InsertionSort(bucket):
+    size = 0
+    for i in range(0, len(bucket)):
+        for j in range(0, len(bucket[i].list)):
+            newList[size] = bucket[i].list[j]
+            size += 1
+
+    return newList
+
+
+def InsertionSort(list):
+    for i in range(1, len(list)):
+        save = list[i]
+        j = i - 1
+        while j >= 0 and list[j] > save:
+            list[j + 1] = list[j]
+            j = j - 1
+
+        list[j + 1] = save
 
 
 if __name__ == "__main__":
     bucket = [Bucket() for i in range(10)]
 
-    intList = [0.78, 0.17, 0.21, 0.26, 0.39, 0.94, 0.21]
-    BucketSort(bucket, intList)
+    intList = [0.78, 0.17, 0.21, 0.26, 0.39, 0.94, 0.2]
+    result = BucketSort(bucket, intList)
 
-    for i in range(0, len(bucket)):
-        print(bucket[i].list)
+    print("Unsorted")
+    for i in range(0, len(result)):
+        print(intList[i], end=" ")
+
+    print("\nSorted")
+    for i in range(0, len(result)):
+        print(result[i], end=" ")
